c6dfc4de71c98a713ca65a95ffa1819eaf75dc8b
Kornel <kornelkrzyzan@gmail.com>
2023-01-28 15:50:42 +0100

    Dodano router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MainContainerComponent} from './main-container/main-container.component';
+import {NavigatorComponent} from './navigator/navigator.component';
+import {ContentContainerComponent} from './content-container/content-container.component';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {PageMainComponent} from './page-main/page-main.component';
+import {PageProductFormComponent} from './page-product-form/page-product-form.component';
+import {PageProductListComponent} from './page-product-list/page-product-list.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    NavigatorComponent,
+    ContentContainerComponent,
+    PageMainComponent,
+    PageProductFormComponent,
+    PageProductListComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    RouterOutlet,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
